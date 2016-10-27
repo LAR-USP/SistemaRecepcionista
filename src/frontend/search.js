5ef9a6f29512c14ee089149e47254d2d2fdc191e
Tarcísio L Oliveira <devtarc@gmail.com>
2016-10-27 16:20:53 -0200

    Adiciona transição para exibição de resultados

--- src/frontend/search.js
+++ src/frontend/search.js
@@ -1,5 +1,7 @@
 $(document).on('input', '#searchbar', function(){
     var str = $("#searchbar").val();
+    $("#searchdiv").css('top', '0%');
+    $("#results").css('top', '10%');
     $("#results").text(str);
 });
 
