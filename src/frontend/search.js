924cde0fb02773802cc631acd221d0009fc8877f
Tarcísio L Oliveira <devtarc@gmail.com>
2016-10-27 16:46:33 -0200

    Adiciona função de envio Ajax do jQuery
    Substitui a ação de debug usada anteriormente, em que a função simplemente
    adicionava ao interior do div #results o que era escrito na barra de
    pesquisa. Com a mudança, o efeito é o mesmo, mas feito por Ajax, para
    teste.

--- src/frontend/search.js
+++ src/frontend/search.js
@@ -2,6 +2,17 @@ $(document).on('input', '#searchbar', function(){
     var str = $("#searchbar").val();
     $("#searchdiv").css('top', '0%');
     $("#results").css('top', '10%');
-    $("#results").text(str);
+    $.ajax({
+            url:'Search',
+            data:{content:str},
+            type:'get',
+            cache:false,
+            success:function(data){
+                $('#results').text(data); 
+            },
+        error:function(){
+            alert('error');
+        }
+    });
 });
 
