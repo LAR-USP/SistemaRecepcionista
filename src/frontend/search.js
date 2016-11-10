55665e0b4bf3b74fcde94b57099393209ad2da77
Tarcísio L Oliveira <devtarc@gmail.com>
2016-11-10 17:39:19 -0200

    Adiciona reconhecimento de voz
    Botão de reconhecimento de voz substitui o botão de envio.

--- src/frontend/search.js
+++ src/frontend/search.js
@@ -16,3 +16,21 @@ $(document).on('input', '#searchbar', function(){
     });
 });
 
+function startDictation() {
+    if (window.hasOwnProperty('webkitSpeechRecognition')) {
+        var recognition = new webkitSpeechRecognition();
+        recognition.continuous = true;
+        recognition.interimResults = false;
+        recognition.lang = "pt-BR";
+        recognition.start();
+        var i = 0;
+        recognition.onresult = function(e) {
+            $('#searchbar').val(e.results[i][0].transcript);
+            $('#searchbar').trigger("input");
+            ++i;
+        };
+        recognition.onerror = function(e) {
+            recognition.stop();
+        };
+    }
+}
