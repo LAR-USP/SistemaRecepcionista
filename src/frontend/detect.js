57d66b47c262f27bbd4fdcad19e4b36c69b04140
Tarcísio L Oliveira <devtarc@gmail.com>
2016-11-13 23:25:45 -0200

    Melhorias no detect.js
    Adiciona função callDetect() para chamar o Kinect.
    
    Adiciona intervalo de tempo para execução de callDetect().

--- src/frontend/detect.js
+++ src/frontend/detect.js
@@ -1,10 +1,11 @@
 // Faz com que o Kinect examine o que observa para
 // detectar a presença de uma pessoa, com intervalos
 // de tempo constantes.
-window.on('load', function(){
-    var str = $("#searchbar").val();
-    $("#searchdiv").css('top', '0%');
-    $("#results").css('top', '10%');
+$(window).on('load', callDetect);
+
+window.setInterval(callDetect, 10*1000);
+
+function callDetect(){
     $.ajax({
             url:'Detect',
             data:{},
@@ -12,10 +13,9 @@ window.on('load', function(){
             cache:false,
             success:function(data){
                 $('#results').text(data); 
-            },
+        },
         error:function(){
             alert('error');
         }
     });
-});
-
+}
