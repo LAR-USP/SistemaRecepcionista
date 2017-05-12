477e761ee9aa07bbb07a96232d7f3de3a243e764
Tarcísio L Oliveira <devtarc@gmail.com>
2017-05-12 17:49:31 -0300

    Adiciona introspecção na LARA
    Permite que usuário pergunte quem ela é e como pesquisar informações
    com ela.

--- src/backend/usp/lar/lara/servlet/Search.java
+++ src/backend/usp/lar/lara/servlet/Search.java
@@ -4,8 +4,10 @@ import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import java.io.IOException;
+import java.text.Collator;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Locale;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import usp.lar.lara.ontology.LaraParser;
@@ -20,6 +22,33 @@ import usp.lar.lara.calendar.CalendarSearch;
  */
 public class Search extends HttpServlet{
     private OntologySearch os = new OntologySearch();
+
+    public JsonObject introspection(String pergunta){
+        JsonObject rv= new JsonObject();
+        String answer = "";
+        String voice = "";
+
+        Collator compare = Collator.getInstance(Locale.getDefault());
+        compare.setStrength(Collator.PRIMARY);
+        compare.setDecomposition(Collator.NO_DECOMPOSITION);
+        if(compare.compare(pergunta, "Quem é você") == 0 ||
+           compare.compare(pergunta, "Quem é você?") == 0){
+            answer = "Olá! <br />Eu sou a Assistente do Laboratório de Aprendizado de Robôs, mas você pode me chamar de LARA. <br />Eu sou uma inteligência artificial que visa prover informações sobre o Centro de Robótica de São Carlos, como professores que o compõem, <br />próximos eventos, dentre outros. <br /><br />Se precisar de algo, é só pedir! Você pode saber mais escrevendo ou dizendo \"Ajuda\".";
+            voice = answer.replaceAll("<br />", "");
+        } else if(compare.compare(pergunta, "Ajuda") == 0){
+            answer = "Você pode procurar sobre professores inserindo seu nome e o que deseja saber. Eu conheço a área de pesquisa, o cargo, a sala, o email, <br />o ramal e o Lattes deles. <br /> Quanto aos eventos do Centro de Robótica, eu sei sobre os eventos de hoje, amanhã, da semana, do mês e do ano, tanto na parte da <br />manhã quanto da tarde e da noite.";
+        }
+        
+        if(!answer.isEmpty()){
+            rv.addProperty("answer", answer);
+            rv.addProperty("voice", voice);
+            rv.addProperty("entity", "");
+            rv.add("properties", new JsonArray());
+            return rv;
+        } else {
+            return null;
+        }
+    }
 
     /**
      * @brief Recebe um envio GET do AJAX em search.js.
@@ -58,12 +87,15 @@ public class Search extends HttpServlet{
         }
 
         JsonObject rv = null;
-        if(LaraParser.requestType(pergunta) == LaraParser.Type.ONTOLOGY){
-            rv = this.os.search(pergunta, entidade, propriedades);
-        } else {
-            rv = CalendarSearch.getList(pergunta);
+        rv = this.introspection(pergunta);
+        if(rv == null){
+            if(LaraParser.requestType(pergunta) == LaraParser.Type.ONTOLOGY){
+                rv = this.os.search(pergunta, entidade, propriedades);
+            } else {
+                rv = CalendarSearch.getList(pergunta);
+            }
         }
-        
+
         response.setCharacterEncoding("UTF-8");
         response.setContentType("application/json");
         response.getWriter().write( g.toJson(rv) );//.getBytes( "UTF-8" ) );
